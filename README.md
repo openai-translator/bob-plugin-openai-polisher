a5b65a47204b24060cd688f9dc6bef34cd3283c1
yetone <yetoneful@gmail.com>
2023-03-03 19:13:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+OpenAI Polisher Bob Plugin
+----------------------------
+
+# 简介
+
+ChatGPT 向我们展示了 GPT 模型的伟大之处，所以我使用 OpenAI 的 API 实现了这个 Bob 的润色 + 语法修改插件，效果拔群！
+
+# 使用截图
+
+![](https://user-images.githubusercontent.com/1206493/221086195-f1ed941d-4dfa-4aa0-9d47-56c258a8f854.gif)
+
+# 使用方法
+
+1. 安装 [Bob](https://bobtranslate.com/guide/#%E5%AE%89%E8%A3%85) (版本 >= 0.50)
+2. 下载此插件: [openai-polisher.bobplugin](https://github.com/yetone/bob-plugin-openai-polisher/releases)
+3. 安装此插件:
+
+![](https://user-images.githubusercontent.com/1206493/219937302-6be8d362-1520-4906-b8d6-284d01012837.gif)
+
+4. 去 [OpenAI](https://platform.openai.com/account/api-keys) 获取你的 API KEY
+5. 把 API KEY 填入 Bob 此插件配置界面的 API KEY
+ 输入框中
+
+![](https://user-images.githubusercontent.com/1206493/219937398-8e5bb8d2-7dc8-404a-96e7-a937e08c939f.gif)
+
+6. 安装 [PopClip](https://bobtranslate.com/guide/integration/popclip.html) 实现划词后鼠标附近出现小图标
+
+![](https://user-images.githubusercontent.com/1206493/219933584-d0c2b6cf-8fa0-40a6-858f-8f4bf05f38ef.gif)
+
+# 请作者喝一杯咖啡
+
+<div align="center">
+<img height="360" src="https://user-images.githubusercontent.com/1206493/220753437-90e4039c-d95f-4b6a-9a08-b3d6de13211f.png" />
+<img height="360" src="https://user-images.githubusercontent.com/1206493/220756036-d9ac4512-0375-4a32-8c2e-8697021058a2.png" />
+</div>
---
+++ src/main.js
@@ -0,0 +1,107 @@
+var lang = require("./lang.js");
+
+function supportLanguages() {
+    return lang.supportLanguages.map(([standardLang]) => standardLang);
+}
+
+function translate(query, completion) {
+    const ChatGPTModels = ["gpt-3.5-turbo", "gpt-3.5-turbo-0301"];
+    const api_keys = $option.api_keys.split(",").map((key) => key.trim());
+    const api_key = api_keys[Math.floor(Math.random() * api_keys.length)];
+    const header = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${api_key}`,
+    };
+    let prompt = "";
+    if (query.detectFrom === "zh-Hant" || query.detectFrom === "zh-Hans") {
+        prompt = "请润色一下这句话";
+    } else {
+        prompt = "please polish this sentence";
+    }
+    const body = {
+        model: $option.model,
+        temperature: 0,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 1,
+        presence_penalty: 1,
+    };
+    const isChatGPTModel = ChatGPTModels.indexOf($option.model) > -1;
+    if (isChatGPTModel) {
+        body.messages = [
+            { role: "system", content: prompt },
+            { role: "user", content: query.text },
+        ];
+    } else {
+        body.prompt = `${prompt}:\n\n"${query.text}" =>`;
+    }
+    (async () => {
+        const resp = await $http.request({
+            method: "POST",
+            url:
+                "https://api.openai.com/v1" +
+                (isChatGPTModel ? "/chat/completions" : "/completions"),
+            header,
+            body,
+        });
+
+        if (resp.error) {
+            const { statusCode } = resp.response;
+            let reason;
+            if (statusCode >= 400 && statusCode < 500) {
+                reason = "param";
+            } else {
+                reason = "api";
+            }
+            completion({
+                error: {
+                    type: reason,
+                    message: `接口响应错误 - ${resp.data.error.message}`,
+                    addtion: JSON.stringify(resp),
+                },
+            });
+        } else {
+            const { choices } = resp.data;
+            if (!choices || choices.length === 0) {
+                completion({
+                    error: {
+                        type: "api",
+                        message: "接口未返回结果",
+                    },
+                });
+                return;
+            }
+            if (isChatGPTModel) {
+                targetTxt = choices[0].message.content.trim();
+            } else {
+                targetTxt = choices[0].text.trim();
+            }
+            if (!isChatGPTModel) {
+                if (targetTxt.startsWith('"')) {
+                    targetTxt = targetTxt.slice(1);
+                }
+                if (targetTxt.endsWith('"')) {
+                    targetTxt = targetTxt.slice(0, -1);
+                }
+            }
+            completion({
+                result: {
+                    from: query.detectFrom,
+                    to: query.detectTo,
+                    toParagraphs: targetTxt.split("\n"),
+                },
+            });
+        }
+    })().catch((err) => {
+        completion({
+            error: {
+                type: err._type || "unknown",
+                message: err._message || "未知错误",
+                addtion: err._addtion,
+            },
+        });
+    });
+}
+
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
