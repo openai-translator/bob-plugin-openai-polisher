4714c502341290a4b14036e434b7df50cf6bac73
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-05-18 17:17:37 +0800

    feat: code refactoring, major updates
    1. Support gpt-4 models
    2. Support Azure OpenAI service
    3. Support custom user prompt

--- src/main.js
+++ src/main.js
@@ -1,136 +1,280 @@
-var lang = require("./lang.js");
+//@ts-check
 
-function supportLanguages() {
-    return lang.supportLanguages.map(([standardLang]) => standardLang);
+var lang = require("./lang.js");
+var ChatGPTModels = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0301",
+    "gpt-4",
+    "gpt-4-0314",
+    "gpt-4-32k",
+    "gpt-4-32k-0314",
+];
+
+/**
+ * @param {string}  url
+ * @returns {string} 
+*/
+function ensureHttpsAndNoTrailingSlash(url) {
+    const hasProtocol = /^[a-z]+:\/\//i.test(url);
+    const modifiedUrl = hasProtocol ? url : 'https://' + url;
+
+    return modifiedUrl.endsWith('/') ? modifiedUrl.slice(0, -1) : modifiedUrl;
 }
 
-function translate(query, completion) {
-    const ChatGPTModels = ["gpt-3.5-turbo", "gpt-3.5-turbo-0301"];
-    const api_keys = $option.api_keys.split(",").map((key) => key.trim());
-    const api_key = api_keys[Math.floor(Math.random() * api_keys.length)];
-    const header = {
+/**
+ * @param {boolean} isAzureServiceProvider - Indicates if the service provider is Azure.
+ * @param {string} apiKey - The authentication API key.
+ * @returns {{
+*   "Content-Type": string;
+*   "api-key"?: string;
+*   "Authorization"?: string;
+* }} The header object.
+*/
+function buildHeader(isAzureServiceProvider, apiKey) {
+    return {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${api_key}`,
+        [isAzureServiceProvider ? "api-key" : "Authorization"]: isAzureServiceProvider ? apiKey : `Bearer ${apiKey}`
     };
-    const detailedPolishingMode = $option.polishing_mode === "detailed";
-    let prompt = $option.prompt;
-    if (detailedPolishingMode) {
-        prompt = `${prompt}. Please note that you need to list the changes and briefly explain why`;
+}
+
+/**
+ * @param {string} basePrompt
+ * @param {"simplicity" | "detailed"} polishingMode
+ * @param {Bob.TranslateQuery} query
+ * @returns {string}
+*/
+function generateSystemPrompt(basePrompt, polishingMode, query) {
+    let systemPrompt = basePrompt || "Revise the following sentences to make them more clear, concise, and coherent.";
+
+    const isDetailedPolishingMode = polishingMode === "detailed";
+    if (isDetailedPolishingMode) {
+        systemPrompt = `${systemPrompt}. Please note that you need to list the changes and briefly explain why`;
     }
     switch (query.detectFrom) {
         case "zh-Hant":
-            prompt = "潤色此句";
-            if (detailedPolishingMode) {
-                prompt = `${prompt}。請列出修改項目，並簡述修改原因`;
+            systemPrompt = "潤色此句";
+            if (isDetailedPolishingMode) {
+                systemPrompt = `${systemPrompt}。請列出修改項目，並簡述修改原因`;
             }
             break;
         case "zh-Hans":
-            prompt = "润色此句";
-            if (detailedPolishingMode) {
-                prompt = `${prompt}。请注意要列出更改以及简要解释一下为什么这么修改`;
+            systemPrompt = "润色此句";
+            if (isDetailedPolishingMode) {
+                systemPrompt = `${systemPrompt}。请注意要列出更改以及简要解释一下为什么这么修改`;
             }
             break;
         case "ja":
-            prompt = "この文章を装飾する";
-            if (detailedPolishingMode) {
-                prompt = `${prompt}。変更点をリストアップし、なぜそのように変更したかを簡単に説明することに注意してください`;
+            systemPrompt = "この文章を装飾する";
+            if (isDetailedPolishingMode) {
+                systemPrompt = `${systemPrompt}。変更点をリストアップし、なぜそのように変更したかを簡単に説明することに注意してください`;
             }
             break;
         case "ru":
-            prompt =
+            systemPrompt =
                 "Переформулируйте следующие предложения, чтобы они стали более ясными, краткими и связными";
-            if (detailedPolishingMode) {
-                prompt = `${prompt}. Пожалуйста, обратите внимание на необходимость перечисления изменений и краткого объяснения причин таких изменений`;
+            if (isDetailedPolishingMode) {
+                systemPrompt = `${systemPrompt}. Пожалуйста, обратите внимание на необходимость перечисления изменений и краткого объяснения причин таких изменений`;
             }
             break;
         case "wyw":
-            prompt = "润色此句古文";
-            if (detailedPolishingMode) {
-                prompt = `${prompt}。请注意要列出更改以及简要解释一下为什么这么修改`;
+            systemPrompt = "润色此句古文";
+            if (isDetailedPolishingMode) {
+                systemPrompt = `${systemPrompt}。请注意要列出更改以及简要解释一下为什么这么修改`;
             }
             break;
         case "yue":
-            prompt = "潤色呢句粵語";
-            if (detailedPolishingMode) {
-                prompt = `${prompt}。記住要列出修改嘅內容同簡單解釋下點解要做呢啲更改`;
+            systemPrompt = "潤色呢句粵語";
+            if (isDetailedPolishingMode) {
+                systemPrompt = `${systemPrompt}。記住要列出修改嘅內容同簡單解釋下點解要做呢啲更改`;
             }
             break;
     }
-    const body = {
-        model: $option.model,
+
+    return systemPrompt;
+}
+
+/**
+ * @param {typeof ChatGPTModels[number]} model
+ * @param {boolean} isChatGPTModel
+ * @param {Bob.TranslateQuery} query
+ * @returns {{ 
+ *  model: typeof ChatGPTModels[number];
+ *  temperature: number;
+ *  max_tokens: number;
+ *  top_p: number;
+ *  frequency_penalty: number;
+ *  presence_penalty: number;
+ *  messages?: {
+ *    role: "system" | "user";
+ *    content: string;
+ *  }[];
+ *  prompt?: string;
+ * }}
+*/
+function buildRequestBody(model, isChatGPTModel, query) {
+    const { customSystemPrompt, customUserPrompt, polishingMode } = $option;
+
+    const systemPrompt = generateSystemPrompt(customSystemPrompt, polishingMode, query);
+    const userPrompt = customUserPrompt ? `${customUserPrompt}:\n\n"${query.text}"` : query.text;
+
+    const standardBody = {
+        model,
         temperature: 0,
         max_tokens: 1000,
         top_p: 1,
         frequency_penalty: 1,
         presence_penalty: 1,
     };
-    const isChatGPTModel = ChatGPTModels.indexOf($option.model) > -1;
+
     if (isChatGPTModel) {
-        body.messages = [
-            { role: "system", content: prompt },
-            { role: "user", content: query.text },
-        ];
-    } else {
-        body.prompt = `${prompt}:\n\n${query.text} =>`;
+        return {
+            ...standardBody,
+            messages: [
+                {
+                    role: "system",
+                    content: systemPrompt,
+                },
+                {
+                    role: "user",
+                    content: userPrompt,
+                },
+            ],
+        };
     }
+    return {
+        ...standardBody,
+        prompt: `${systemPrompt}\n\n${userPrompt}`,
+    };
+}
+
+/**
+ * @param {Bob.Completion} completion
+ * @param {Bob.HttpResponse} result
+ * @returns {void}
+*/
+function handleError(completion, result) {
+    const { statusCode } = result.response;
+    const reason = (statusCode >= 400 && statusCode < 500) ? "param" : "api";
+    completion({
+        error: {
+            type: reason,
+            message: `接口响应错误 - ${result.data.error.message}`,
+            addtion: JSON.stringify(result),
+        },
+    });
+}
+
+/**
+ * @param {Bob.Completion} completion
+ * @param {boolean} isChatGPTModel
+ * @param {Bob.TranslateQuery} query
+ * @param {Bob.HttpResponse} result
+ * @returns {void}
+*/
+function handleResponse(completion, isChatGPTModel, query, result) {
+    const { choices } = result.data;
+
+    if (!choices || choices.length === 0) {
+        completion({
+            error: {
+                type: "api",
+                message: "接口未返回结果",
+                addtion: JSON.stringify(result),
+            },
+        });
+        return;
+    }
+
+    let targetText = (isChatGPTModel ? choices[0].message.content : choices[0].text).trim();
+
+    completion({
+        result: {
+            from: query.detectFrom,
+            to: query.detectTo,
+            toParagraphs: targetText.split("\n"),
+        },
+    });
+}
+
+/**
+ * @type {Bob.Translate}
+ */
+function translate(query, completion) {
+    if (!lang.langMap.get(query.detectTo)) {
+        completion({
+            error: {
+                type: "unsupportLanguage",
+                message: "不支持该语种",
+                addtion: "不支持该语种",
+            },
+        });
+    }
+
+    const { model, apiKeys, apiUrl, deploymentName } = $option;
+
+    if (!apiKeys) {
+        completion({
+            error: {
+                type: "secretKey",
+                message: "配置错误 - 请确保您在插件配置中填入了正确的 API Keys",
+                addtion: "请在插件配置中填写 API Keys",
+            },
+        });
+    }
+    const trimmedApiKeys = apiKeys.endsWith(",") ? apiKeys.slice(0, -1) : apiKeys;
+    const apiKeySelection = trimmedApiKeys.split(",").map(key => key.trim());
+    const apiKey = apiKeySelection[Math.floor(Math.random() * apiKeySelection.length)];
+
+    const modifiedApiUrl = ensureHttpsAndNoTrailingSlash(apiUrl || "https://api.openai.com");
+    
+    const isChatGPTModel = ChatGPTModels.includes(model);
+    const isAzureServiceProvider = modifiedApiUrl.includes("openai.azure.com");
+    let apiUrlPath = isChatGPTModel ? "/v1/chat/completions" : "/v1/completions";
+    
+    if (isAzureServiceProvider) {
+        if (deploymentName) {
+            apiUrlPath = `/openai/deployments/${deploymentName}`;
+            apiUrlPath += isChatGPTModel ? "/chat/completions?api-version=2023-03-15-preview" : "/completions?api-version=2022-12-01";
+        } else {
+            completion({
+                error: {
+                    type: "secretKey",
+                    message: "配置错误 - 未填写 Deployment Name",
+                    addtion: "请在插件配置中填写 Deployment Name",
+                },
+            });
+        } 
+    }
+
+    const header = buildHeader(isAzureServiceProvider, apiKey);
+    const body = buildRequestBody(model, isChatGPTModel, query);
+
     (async () => {
-        const resp = await $http.request({
+        const result = await $http.request({
             method: "POST",
-            url:
-                $option.api_url +
-                (isChatGPTModel ? "/v1/chat/completions" : "/v1/completions"),
+            url: modifiedApiUrl + apiUrlPath,
             header,
             body,
         });
 
-        if (resp.error) {
-            const { statusCode } = resp.response;
-            let reason;
-            if (statusCode >= 400 && statusCode < 500) {
-                reason = "param";
-            } else {
-                reason = "api";
-            }
-            completion({
-                error: {
-                    type: reason,
-                    message: `接口响应错误 - ${resp.data.error.message}`,
-                    addition: JSON.stringify(resp),
-                },
-            });
+        if (result.error) {
+            handleError(completion, result);
         } else {
-            const { choices } = resp.data;
-            if (!choices || choices.length === 0) {
-                completion({
-                    error: {
-                        type: "api",
-                        message: "接口未返回结果",
-                    },
-                });
-                return;
-            }
-            if (isChatGPTModel) {
-                targetTxt = choices[0].message.content.trim();
-            } else {
-                targetTxt = choices[0].text.trim();
-            }
-            completion({
-                result: {
-                    from: query.detectFrom,
-                    to: query.detectTo,
-                    toParagraphs: targetTxt.split("\n"),
-                },
-            });
+            handleResponse(completion, isChatGPTModel, query, result);
         }
     })().catch((err) => {
         completion({
             error: {
                 type: err._type || "unknown",
                 message: err._message || "未知错误",
-                addition: err._addition,
+                addtion: err._addition,
             },
         });
     });
+}
+
+function supportLanguages() {
+    return lang.supportLanguages.map(([standardLang]) => standardLang);
 }
 
 exports.supportLanguages = supportLanguages;
